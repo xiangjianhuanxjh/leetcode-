1c8b5eaf74d894c2f7d08eb02db42c2b6a6a27df
幸家豪 <92107319+xiangjianhuanxjh@users.noreply.github.com>
2023-03-22 17:50:21 +0800

    Create 13.罗马数字转整数.cpp

---
+++ 13.罗马数字转整数.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int res = 0;
+       const char *arr = NULL;
+       arr = s.c_str();
+        for(int i=s.length() - 1;i>=0;i--)
+        {
+            char c=arr[i];
+            switch(c)
+            {
+                case 'I':
+                    res +=(res>=5?-1:1);
+                    break;
+                case 'V':
+                    res +=5;
+                    break;
+                case 'X':
+                    res +=10*(res >=50?-1:1);
+                    break;
+                case 'L':
+                    res+=50;
+                    break;
+                case 'C':
+                    res+=100*(res>=500?-1:1);
+                    break;
+                case 'D':
+                res +=500;
+                break;
+                case 'M':
+                res+=1000;
+                break;
+            }
+        }
+        return res;
+    }
+};
